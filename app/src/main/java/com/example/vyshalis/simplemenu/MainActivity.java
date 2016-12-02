30eeeeef8d5f01f25cbb8fa05c57152a8748aa7d
Vyshali <vyshalisatiz@gmail.com>
2016-12-02 13:54:13 +0530

    Option menu

--- app/src/main/java/com/example/vyshalis/simplemenu/MainActivity.java
+++ app/src/main/java/com/example/vyshalis/simplemenu/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,27 +13,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-   // @Override
+    @Override
 
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
+                inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
     }
 
-  //  @Override
 
-    public boolean onCreateOptionsMenu(android.view.MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item_share:
-                Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.item_delete:
-                Toast.makeText(MainActivity.this, "Delete", Toast.LENGTH_SHORT).show();
-                return true;
-
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-}
