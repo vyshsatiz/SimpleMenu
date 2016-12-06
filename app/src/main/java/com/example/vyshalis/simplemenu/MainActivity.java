1bc41da8f76efe55c5a159794cc194d3bf33e4c1
Vyshali <vyshalisatiz@gmail.com>
2016-12-06 22:05:51 +0530

    Menu options added

--- app/src/main/java/com/example/vyshalis/simplemenu/MainActivity.java
+++ app/src/main/java/com/example/vyshalis/simplemenu/MainActivity.java
@@ -1,28 +1,63 @@
 package com.example.vyshalis.simplemenu;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     @Override
 
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
 
-        MenuInflater inflater = getMenuInflater();
-                inflater.inflate(R.menu.menu, menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.my_menu, menu);
 
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        RelativeLayout relativeLayout;
+        relativeLayout = (RelativeLayout) findViewById(R.id.relative_layout);
+        switch (item.getItemId())
+
+        {
+
+            case R.id.id_blue_color:
+                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+                break;
+
+            case R.id.id_red_color:
+                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+
+                break;
+
+            case R.id.id_green_color:
+                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+                break;
+
+            case R.id.id_purple_color:
+                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+
+                break;
+
+            case R.id.id_yellow_color:
+                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+
+                break;
+        }
+
+        return true;
     }
+}
+
 
 
