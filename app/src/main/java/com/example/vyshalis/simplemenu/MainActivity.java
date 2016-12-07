4cfd54c5b3bd56f50de20dd7ce92ece4440c5554
Vyshali <vyshalisatiz@gmail.com>
2016-12-07 13:06:41 +0530

    Final commit

--- app/src/main/java/com/example/vyshalis/simplemenu/MainActivity.java
+++ app/src/main/java/com/example/vyshalis/simplemenu/MainActivity.java
@@ -8,12 +8,11 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
-
     public boolean onCreateOptionsMenu(Menu menu) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
+         setContentView(R.layout.activity_main);
 
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.my_menu, menu);
@@ -25,32 +24,26 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         RelativeLayout relativeLayout;
         relativeLayout = (RelativeLayout) findViewById(R.id.relative_layout);
+
         switch (item.getItemId())
 
         {
 
             case R.id.id_blue_color:
+
+                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+
+                break;
+
+            case R.id.id_pink_color:
+
                 relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorAccent));
 
                 break;
 
-            case R.id.id_red_color:
-                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-
-                break;
-
-            case R.id.id_green_color:
-                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-
-                break;
-
-            case R.id.id_purple_color:
-                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-
-                break;
-
             case R.id.id_yellow_color:
-                relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+
+               relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
 
                 break;
         }
